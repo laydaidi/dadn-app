39671e44d2ddfe83b0768756dbc5711ce316dc37
Bui Ngo Hoang Long <buingohoanglong2000@gmail.com>
2021-05-30 12:05:09 +0700

    load lesson video

--- app/src/main/java/com/example/dadn_app/activities/LibraryVideoActivity.java
+++ app/src/main/java/com/example/dadn_app/activities/LibraryVideoActivity.java
@@ -2,31 +2,47 @@ package com.example.dadn_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.dadn_app.R;
 import com.example.dadn_app.helpers.Helper;
 import com.example.dadn_app.helpers.Record;
 import com.example.dadn_app.helpers.RecordAdapter;
+import com.example.dadn_app.helpers.VideoStreamVolleyRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -35,6 +51,8 @@ public class LibraryVideoActivity extends AppCompatActivity {
     TextView textViewName;
     TextView textViewDescription;
     VideoView videoView;
+
+    File videoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +91,7 @@ public class LibraryVideoActivity extends AppCompatActivity {
                         if (jsonObject != null) {
                             textViewName.setText(jsonObject.getString("name"));
                             textViewDescription.setText(jsonObject.getString("description"));
-                            videoView.setVideoURI(Uri.parse(jsonObject.getString("url")));
+                            this.loadVideo(jsonObject.getString("url"));
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -115,6 +133,52 @@ public class LibraryVideoActivity extends AppCompatActivity {
             }
         };
         queue.add(request);
+    }
+
+
+    private void loadVideo(String videoUrl) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Helper.buildAPIURL("/video/" + videoUrl);
+        Log.d("LESSON URL: ", url);
+        VideoStreamVolleyRequest request = new VideoStreamVolleyRequest(Request.Method.GET, url,
+                new Response.Listener<byte[]>() {
+                    @Override
+                    public void onResponse(byte[] response) {
+                        // TODO handle the response
+                        try {
+                            if (response!=null) {
+                                String filePrefix = videoUrl.substring(0, videoUrl.lastIndexOf("."));
+                                String fileSuffix = videoUrl.substring(videoUrl.lastIndexOf(".") + 1);
+                                File tempVideo = File.createTempFile(filePrefix, fileSuffix, getCacheDir());
+                                tempVideo.deleteOnExit();
+                                FileOutputStream fos = new FileOutputStream(tempVideo);
+                                fos.write(response);
+                                fos.close();
+
+                                videoView.setVideoPath(tempVideo.getAbsolutePath());
+                                MediaController mediaController = new MediaController(LibraryVideoActivity.this);
+                                videoView.setMediaController(mediaController);
+                                mediaController.setMediaPlayer(videoView);
+                                videoView.setVisibility(View.VISIBLE);
+                                videoView.start();
+                            }
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            Log.d("KEY_ERROR", "UNABLE TO DOWNLOAD FILE");
+                            e.printStackTrace();
+                        }
+                    }
+                } ,new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO handle the error
+                error.printStackTrace();
+            }
+        }, null);
+
+        queue.add(request);
+
     }
 
     private void switchToLogin() {
